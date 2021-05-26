86031b6e2a93dfd5788fc6bc0c454dbd3f35ae19
GolzitskyNikolay <golzitskij.ns@edu.spbstu.ru>
2021-05-26 12:57:03 +0300

    added first bindings tests

---
+++ speac_tests/speac_cl4ppy_tests/cl4py_test.py
@@ -0,0 +1,114 @@
+import copy
+import glob
+import unittest
+
+import cl4py
+import os
+
+import coverage
+
+from speac_library.speac.chopin_33_3 import CHOPIN_33_3
+from speac_library.speac.speac_settings import SpeacSettings
+from speac_library.speac.top_level import get_the_levels
+
+lisp = cl4py.Lisp()
+cl = lisp.function("find-package")("CL")
+
+
+def load_files():
+    path = os.path.join(os.path.dirname(__file__), "lisp_files/*.lisp")
+    print(path)
+
+    for file_name in glob.glob(path):
+        file_name = file_name.replace("\\", "/")
+        lisp.eval(("load", '"' + file_name + '"'))
+        print(file_name, " is loaded")
+
+
+def cl4py_elements_to_python(cl4py_element):
+    if isinstance(cl4py_element, cl4py.Symbol):
+        return str(cl4py_element.name).lower()
+
+    elif isinstance(cl4py_element, cl4py.Cons):
+        result = []
+        for element in cl4py_element:
+            result.append(cl4py_elements_to_python(element))
+        return result
+
+    else:
+        return cl4py_element
+
+
+def set_lisp_variable(variable_name, number):
+    lisp.eval(("setq", variable_name, number))
+    print("Lisp variable " + variable_name + " = ", number)
+
+
+class BindingsTest(unittest.TestCase):
+
+    def test_all(self):
+        cov = coverage.Coverage()
+        cov.start()
+
+        load_files()
+
+        self.test_get_the_levels()
+
+        cov.stop()
+        cov.save()
+        cov.report(show_missing=True, include="*/speac/*")
+
+    def test_get_the_levels(self):
+        meter = 3
+        beat = 1000
+        cadence_minimum = 9000
+        intevals_off = 2
+        measures = 8
+        threshold = 2
+        pattern_size = 12
+        amount_off = 1
+        matching_line = 1
+
+        speac_settings = SpeacSettings()
+        speac_settings.set_beat(beat)
+        speac_settings.set_cadence_minimum(cadence_minimum)
+        speac_settings.set_intervals_off(intevals_off)
+        speac_settings.set_measures(measures)
+        speac_settings.set_threshold(threshold)
+        speac_settings.set_pattern_size(pattern_size)
+        speac_settings.set_amount_off(amount_off)
+        speac_settings.set_matching_line(matching_line)
+
+        python_input = CHOPIN_33_3
+        lisp_input = "chopin-33-3"
+        set_lisp_variable("*meter*", meter)
+
+        try:
+            lisp_eval = lisp.eval(("get-the-levels", lisp_input))
+            lisp_result = cl4py_elements_to_python(lisp_eval)
+
+        except Exception as lisp_exception:
+            lisp_result = "Error"
+            # print("++++++++++++++++++++++++++++++++++++++++++")
+            # print("Exception in Lisp result:", lisp_exception)
+            # print("++++++++++++++++++++++++++++++++++++++++++")
+
+        try:
+            python_result = get_the_levels(python_input, meter, speac_settings)
+
+        except Exception as python_exception:
+            python_result = "Error"
+            # print("Exception in Python result: ", python_exception)
+
+        print("  Lisp input = ", lisp_input, "\nPython input = ", python_input,
+              "\n  Lisp result = ", lisp_result, "\nPython result = ", python_result)
+
+        try:
+            self.assertEqual(python_result, lisp_result)
+        except AssertionError:
+            raise AssertionError("Data aren't equal")
+
+
+if __name__ == '__main__':
+    test = BindingsTest()
+    test.test_all()
